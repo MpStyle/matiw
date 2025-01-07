aaa82deebb1b138c165ddd94513723b3844d4534
Michele Pagnin <MpStyle@users.noreply.github.com>
2025-01-07 02:09:33 +0100

    Added redux persiste for custom location and settings

--- src/main.tsx
+++ src/main.tsx
@@ -2,19 +2,22 @@ import {createRoot} from 'react-dom/client'
 import './index.css'
 import {Home} from "./modules/home/components/Home.tsx";
 import {Provider} from "react-redux";
-import {store} from "./modules/core/AppStore.ts";
+import {persistor, store} from "./modules/core/AppStore.ts";
 import {CssBaseline} from "@mui/material";
 import {StrictMode} from "react";
 import {LocalizationProvider} from "@mui/x-date-pickers";
 import {AdapterMoment} from "@mui/x-date-pickers/AdapterMoment";
+import {PersistGate} from 'redux-persist/integration/react'
 
 createRoot(document.getElementById('root')!).render(
     <StrictMode>
         <Provider store={store}>
-            <LocalizationProvider dateAdapter={AdapterMoment}>
-                <CssBaseline/>
-                <Home/>
-            </LocalizationProvider>
+            <PersistGate loading={null} persistor={persistor}>
+                <LocalizationProvider dateAdapter={AdapterMoment}>
+                    <CssBaseline/>
+                    <Home/>
+                </LocalizationProvider>
+            </PersistGate>
         </Provider>
     </StrictMode>,
 )
--- src/modules/core/AppStore.ts
+++ src/modules/core/AppStore.ts
@@ -1,20 +1,32 @@
-import {configureStore} from '@reduxjs/toolkit'
+import {combineReducers, configureStore} from '@reduxjs/toolkit'
 import {homeSlice} from "../home/slices/HomeSlice.ts";
 import {settingsSlice} from "../settings/slices/SettingsSlice.ts";
 import {filtersSlice} from "../filters/slices/FiltersSlice.ts";
 import {customLocationSlice} from "../custom-location/slices/CustomLocationSlice.ts";
+import storage from 'redux-persist/lib/storage' // defaults to localStorage for web
+import {persistReducer, persistStore} from 'redux-persist'
 import {AppState} from "./entities/AppState.ts";
 
-export const store = configureStore<AppState>({
-    reducer: {
-        home: homeSlice.reducer,
-        filters: filtersSlice.reducer,
-        settings: settingsSlice.reducer,
-        customLocations: customLocationSlice.reducer
-    }
-})
+const persistConfig = {
+    key: "matiw",
+    storage,
+    whitelist: ["settings", "customLocations"]
+};
 
-// Infer the `RootState` and `AppDispatch` types from the store itself
-export type RootState = ReturnType<typeof store.getState>
-// Inferred type: {posts: PostsState, comments: CommentsState, users: UsersState}
-export type AppDispatch = typeof store.dispatch
+const persistedReducer = persistReducer<AppState>(persistConfig, combineReducers({
+    home: homeSlice.reducer,
+    filters: filtersSlice.reducer,
+    settings: settingsSlice.reducer,
+    customLocations: customLocationSlice.reducer
+}));
+export const store = configureStore({
+    reducer: persistedReducer,
+    middleware: (getDefaultMiddleware) =>
+        getDefaultMiddleware({
+            serializableCheck: {
+                ignoredActions: ["persist/PERSIST", "persist/REHYDRATE"],
+            },
+        }),
+});
+
+export const persistor = persistStore(store);
